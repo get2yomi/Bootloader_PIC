86324850d6fb824a602f7d52cf10dc5e9749f913
get2yomi <get2kunle@yahoo.co.uk>
2023-11-10 22:40:32 -0500

    Bootloader first commit

---
+++ Bootloader.c
@@ -0,0 +1,165 @@
+
+#include <xc.h>
+#include"pic16f877"
+#include <stdint.h>
+
+// Configuration settings for the PIC16F887 (modify as needed)
+__CONFIG(FOSC_HS & WDTE_OFF & PWRTE_OFF & CP_OFF);
+
+// UART communication settings
+#define UART_BAUDRATE 9600
+#define UART_TX_PIN   TRISC6
+#define UART_RX_PIN   TRISC7
+
+// Define memory addresses for firmware update area
+#define BOOTLOADER_START_ADDRESS 0x2000 // This is just a placeholder
+#define APPLICATION_START_ADDRESS 0x0000 // This is the starting address of your application
+
+void main() {
+    // Check if a firmware update is requested (e.g., based on an external condition)
+    if (firmwareUpdateRequested()) {
+        // Execute firmware update process
+        firmwareUpdate();
+    } else {
+        // Jump to the application if no update is requested
+        gotoApplication();
+    }
+}
+
+// Function to check if a firmware update is requested (e.g., through a button press or external signal)
+int firmwareUpdateRequested() {
+    // Implement your logic here
+    return 0;
+}
+
+// Function to perform the firmware update
+void firmwareUpdate() {
+    // Implement the update process, which involves receiving new firmware data and writing it to the application area
+// Function to initialize UART communication
+void UART_Initialize() {
+    SPBRG = (unsigned char)((_XTAL_FREQ / (16 * UART_BAUDRATE)) - 1);
+    TXSTAbits.SYNC = 0;
+    TXSTAbits.BRGH = 1;
+    RCSTAbits.SPEN = 1;
+    TXSTAbits.TXEN = 1;
+    RCSTAbits.CREN = 1;
+}
+
+
+    // Wait for incoming firmware data
+    while (1) {
+        receiveAndWriteFirmware();
+    
+}
+
+    // After updating, reset the microcontroller or jump to the application
+    resetMicrocontroller();
+}
+
+// Function to receive and write firmware data
+void receiveAndWriteFirmware() {
+    // Implement firmware reception and writing logic here
+// Function to write firmware data to Flash memory
+void writeFirmwareToFlash(uint16_t address, uint8_t* data, uint16_t length) {
+    // Unlock the Flash memory for writing
+    INTCONbits.GIE = 0; // Disable interrupts
+    EECON2 = 0x55;
+    EECON2 = 0xAA;
+    EECON1bits.WR = 1; // Start the write operation
+    INTCONbits.GIE = 1; // Re-enable interrupts
+
+    // Wait for the write operation to complete
+    while (EECON1bits.WR)
+        ;
+
+    // Copy the firmware data to the specified Flash memory address
+    for (uint16_t i = 0; i < length; i++) {
+        asm("DISI #5"); // Disable interrupts during write
+        TBLPTR = address + i;
+        TABLAT = data[i];
+        asm("TBLWTPOSTINC"); // Write to Flash and post-increment address
+    }
+
+    // Write the buffer to Flash
+    asm("DISI #5");
+    EECON1 = 0x04; // Load control register for Write
+    asm("DISI #5");
+    asm("MOVLW 0x55");
+    asm("MOVWF EECON2");
+    asm("DISI #5");
+    asm("MOVLW 0xAA");
+    asm("MOVWF EECON2");
+    asm("DISI #5");
+    EECON1bits.WR = 1; // Start the write operation
+
+    // Wait for the write operation to complete
+    while (EECON1bits.WR)
+        ;
+}
+
+// Function to receive and write firmware data
+void receiveAndWriteFirmware() {
+    uint8_t receivedData[64]; // Buffer for receiving firmware data
+    uint16_t firmwareAddress = 0x2000; // Start address in Flash memory
+    uint16_t dataIndex = 0;
+    uint8_t receivedByte;
+
+    // Implement UART receive logic
+    // Wait for the start of a firmware update packet (e.g., a special header or synchronization sequence)
+
+    while (1) {
+        // Receive data over UART
+        // For example, you might use a function like UART_ReadByte() to receive a byte
+        // Implement your own logic for reading data from the UART
+
+        receivedByte = UART_ReadByte(); // Replace with your UART receive function
+
+        // Check for the end of the firmware data
+        if (receivedByte == 0xFF) {
+            break;
+        }
+
+        // Store received data in the buffer
+        receivedData[dataIndex] = receivedByte;
+        dataIndex++;
+
+        // If the buffer is full, write the data to Flash memory and reset the index
+        if (dataIndex == sizeof(receivedData)) {
+            writeFirmwareToFlash(firmwareAddress, receivedData, sizeof(receivedData));
+            firmwareAddress += sizeof(receivedData);
+            dataIndex = 0;
+        }
+    }
+
+    // Write any remaining data to Flash memory
+    if (dataIndex > 0) {
+        writeFirmwareToFlash(firmwareAddress, receivedData, dataIndex);
+    }
+}
+
+
+}
+
+// Function to jump to the application code
+void gotoApplication() {
+    asm("GOTO " _STR(APPLICATION_START_ADDRESS));
+}
+
+// Function to reset the microcontroller
+void resetMicrocontroller() {
+    asm("RESET");
+}
+
+void interrupt high_priority HighIsr(void) {
+    // Implement your interrupt handling code here
+}
+
+void interrupt low_priority LowIsr(void) {
+    // Implement your interrupt handling code here
+}
+
+
+
+
+
+
